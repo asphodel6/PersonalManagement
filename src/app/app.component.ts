9f2774c5982d5bf62fe7fe5eca9cf6f7bd9a061b
VenchasS <veniy.ru@yandex.ru>
2023-04-03 23:51:09 +0500

    Venchass
    add eslint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'personal_management';
+  public title: string = 'personal_management';
 }
