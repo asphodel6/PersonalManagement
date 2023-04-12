6c7f1cf57e6f68b396df1a034d86e8c1a2e8c123
VenchasS <veniy.ru@yandex.ru>
2023-04-12 17:03:40 +0500

    Vencahss
    create sidebar add simple routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { MatInputModule } from '@angular/material/input';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { RouterModule, Routes  } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { CreateUserComponent } from './create-user/create-user.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ButtonComponent } from './components/button/button.component';
+
+const approutes: Routes = [
+  { path: '', component: UsersComponent },
+  { path: 'createuser', component: CreateUserComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    UsersComponent,
+    CreateUserComponent,
+    NotFoundComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    RouterModule.forRoot(approutes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, SidebarComponent]
 })
 export class AppModule { }
---
+++ src/app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input } from '@angular/core';
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent {
+  // eslint-disable-next-line @typescript-eslint/typedef
+  public value = 'Clear me';
+}
