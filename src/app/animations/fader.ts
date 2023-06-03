2115d9ca20364f26a9e6376ded844f18fbd0bb8f
VenchasS <veniy.ru@yandex.ru>
2023-06-03 19:26:58 +0500

    comments delete

--- src/app/animations/fader.ts
+++ src/app/animations/fader.ts
@@ -3,17 +3,13 @@ import {
   transition,
   style,
   query,
-  group,
-  animateChild,
   animate,
-  keyframes,
   AnimationTriggerMetadata,
 } from '@angular/animations';
 
 export const fader: AnimationTriggerMetadata = 
 trigger('routeAnimations', [
   transition('* <=> *', [
-    // Set a default  style for enter and leave
     query(':enter, :leave', [
       style({
         position: 'absolute',
@@ -23,7 +19,6 @@ trigger('routeAnimations', [
         transform: 'scale(0) translateY(100%)',
       }),
     ]),
-    // Animate the new page in
     query(':enter', [
       animate('300ms ease', style({ opacity: 1, transform: 'scale(1) translateY(0)' })),
     ])
